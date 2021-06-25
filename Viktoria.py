934e08737143da14635e0807b84c7bbfe65ce8ef
microwolnovka27 <86230211+microwolnovka27@users.noreply.github.com>
2021-06-25 15:41:53 +0300

    Update Viktoria.py

--- Viktoria.py
+++ Viktoria.py
@@ -22,20 +22,26 @@ pg.display.update() #обновление экрана
 
 while True:
 
- for i in pg.event.get(): #в пг из папки ивент фн гет, присваиает перем зн
-  if i.type == pg.QUIT:
-    sys.exit()  #кнц
-  elif i.type == pg.KEYDOWN:
-    if i.key == pg.K_LEFT:
-                x -= 3
-    elif i.key == pg.K_RIGHT:
-                x += 3
+    for i in pg.event.get():  # в пг из папки ивент фн гет, присваиает перем зн
+        if i.type == pg.QUIT:
+            sys.exit()  # кнц
+        elif i.type == pg.KEYDOWN:
+            if i.key == pg.K_LEFT:
+                x -= 15
+            elif i.key == pg.K_RIGHT:
+                x += 15
+            elif i.key == pg.K_UP:
+                y -= 15
+            elif i.key == pg.K_DOWN:
+                y += 15
 
- pg.draw.rect(sc, PUR, (x, y, 60, 60), 8)
- clock.tick(FPS) #задержка
+
+    sc.fill((0,0,0))
+    pg.draw.rect(sc, PUR, (x, y, 60, 60), 8)
+    clock.tick(FPS)  # задержка
 
 
-  pg.display.update()
+    pg.display.update()
 
 
 
