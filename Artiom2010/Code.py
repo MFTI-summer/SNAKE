cd09872a3850cb3e3c6f323873621ce6297678b1
Artiom2010 <86230451+Artiom2010@users.noreply.github.com>
2021-06-25 17:58:59 +0300

    Update Code.py

--- Artiom2010/Code.py
+++ Artiom2010/Code.py
@@ -14,15 +14,30 @@ clock = pygame.time.Clock()
 area = pg.image.load("Area.png")
 snake = pg.image.load("Snake.bmp")
 screen = pygame.display.set_mode((w, h))
-x = 100
-y = 100
-
+x = 875
+y = 375
+spedx = 0
+spedy = 0
+PUR = (255, 255, 255)
+sc = pg.display.set_mode ((w,h))
+pygame.draw.rect(sc, PUR, (x, y, 50, 50), 8)
 game = True
 while game:
     screen.blit(area, (500, 100))
     screen.blit(snake, (875, 375))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
             game = False
+        elif i.type == pg.KEYDOWN:
+                if i.key == pg.K_LEFT:
+                    x -= 15
+                elif i.key == pg.K_RIGHT:
+                    x += 15
+                elif i.key == pg.K_UP:
+                    y -= 15
+                elif i.key == pg.K_DOWN:
+                    y += 15
+    sc.fill((0,0,0))
+    pygame.draw.rect(sc, PUR, (x, y, 50, 50), 8)
     pygame.display.update()
 pygame.quit()
