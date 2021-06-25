417731e2cb68dc5cceb630aa7137a13f44717697
Artiom2010 <86230451+Artiom2010@users.noreply.github.com>
2021-06-25 13:41:29 +0300

    ллл
    щзщз

--- Artiom2010/Code.py
+++ Artiom2010/Code.py
@@ -5,7 +5,6 @@ import sys
 pygame.init()
 pygame.mixer.init()
 pygame.mixer.music.load("anotherday.ogg")
-pygame.mixer.music.load("darkside.ogg")
 pygame.mixer.music.set_volume(0.1)
 pygame.mixer.music.play(loops=-1)
 WIDTH = 800
@@ -13,14 +12,12 @@ HEIGHT = 600
 FPS = 60
 area = pg.image.load("Area.bmp")
 screen = pygame.display.set_mode((1680,1020))
-pygame.display.set_caption("Area.bmp")
-clock = pygame.time.Clock()
 x = 100
 y = 100
+screen.blit(area, (300, 300))
 game = True
 while game:
     for event in pygame.event.get():
-        screen.blit(area, (x, y))
         if event.type == pygame.QUIT:
             game = False
 pygame.quit()
