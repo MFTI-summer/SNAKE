93d7ed452fde2116f03c1eb9793cea2d5f8b7165
Ekaterina Divina <ekaterinadivina@gmail.com>
2021-06-25 17:56:17 +0300

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,23 +1,29 @@
 import pygame as pg
 import sys
+
 #### from pygame.locals import * - чтобы меньше писать
 
-FPS = 60
-W = 1000 #ширина
-H = 800 #высота
-x = 480
-y = 350
-PUR = (100, 128, 255)
 
 pg.init()
-sc = pg.display.set_mode ((W,H)) #длина высота окна
-pg.draw.rect(sc, PUR, (x, y, 60, 60), 8)
 
+### ЭКРАН
+W = 1000  # ширина
+H = 800  # высота
+sc = pg.display.set_mode((W, H))  # длина высота окна
+
+# FPS
+FPS = 60
 clock = pg.time.Clock()
 
+# SNAKE HEAD ГОЛОВА ЗМЕИ
+x = 480
+y = 350
+PUR = (100, 128, 255)  #ЦВЕТ ЗМЕИ
+
+speed_x = 0
+speed_y = 0
 
 
-pg.display.update() #обновление экрана
 
 
 while True:
@@ -35,13 +41,8 @@ while True:
             elif i.key == pg.K_DOWN:
                 y += 15
 
-
-    sc.fill((0,0,0))
+    sc.fill((0, 0, 0))
     pg.draw.rect(sc, PUR, (x, y, 60, 60), 8)
     clock.tick(FPS)  # задержка
 
-
-    pg.display.update()
-
-
-
+    pg.display.update()  # обновление экрана
