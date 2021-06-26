d5b8300050ca0c4231f02a53702ea751cca82e93
Artiom2010 <86230451+Artiom2010@users.noreply.github.com>
2021-06-26 22:51:20 +0300

    i
    i

---
+++ Artiom2010/Mixer.py
@@ -0,0 +1,36 @@
+import pygame
+import random
+import pygame as pg
+import sys
+
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.set_volume(0.1)
+pygame.mixer.music.load("musictest1.ogg")
+pygame.mixer.music.load("musictest2.ogg")
+pygame.mixer.music.load("musictest3.ogg")
+pygame.mixer.music.load("musictest4.ogg")
+pygame.mixer.music.queue("musictest1.ogg")
+pygame.mixer.music.queue("musictest2.ogg")
+pygame.mixer.music.queue("musictest3.ogg")
+pygame.mixer.music.queue("musictest4.ogg")
+pygame.mixer.music.play(loops=-1)
+w = 1680
+h = 1020
+FPS = 60
+clock = pg.time.Clock()
+
+
+apple = pygame.image.load("apple.png")
+
+screen = pygame.display.set_mode((w, h))
+x = 825
+y = 400
+
+game = True
+while game:
+    screen.blit(apple, (x, y))
+    pg.display.update()
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            game = False
