42078a126b76c95c785c2b6dfe7f80ad752a4947
Artiom2010 <86230451+Artiom2010@users.noreply.github.com>
2021-06-26 16:16:52 +0300

    Update AppleDisplay.py

--- Artiom2010/AppleDisplay.py
+++ Artiom2010/AppleDisplay.py
@@ -1,1 +1,50 @@
+import pygame
+import random
+import pygame as pg
+import sys
+
+pygame.init()
+pygame.mixer.init()
+
+w = 1680
+h = 1020
+FPS = 60
+clock = pg.time.Clock()
+
+area = pg.image.load("Area.png")
+apple = pg.image.load("apple.png")
+snake = pg.image.load("Snake.bmp")
+screen = pygame.display.set_mode((w, h))
+
+x = 875
+y = 375
+
+game = True
+while game:
+    screen.blit(apple, (400, 200))
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            game = False
+
+pg.display.update()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
