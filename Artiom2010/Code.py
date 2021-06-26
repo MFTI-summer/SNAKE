e756c133b07228cde551c6f600cb04d0798c90ee
Artiom2010 <86230451+Artiom2010@users.noreply.github.com>
2021-06-26 13:32:05 +0300

    лл
    ллл

--- Artiom2010/Code.py
+++ Artiom2010/Code.py
@@ -10,13 +10,13 @@ pygame.mixer.music.set_volume(0.1)
 pygame.mixer.music.play(loops=-1)
 
 
-WIDTH = 1680
-HEIGHT = 1020
+w = 1680
+h = 1020
 FPS = 60
 
 clock = pygame.time.Clock()
 area = pg.image.load("Area.png")
-
+apple = pg.image.load("apple.png")
 snake = pg.image.load("Snake.bmp")
 screen = pygame.display.set_mode((w, h))
 x = 875
@@ -30,10 +30,11 @@ game = True
 while game:
     screen.blit(area, (500, 100))
     screen.blit(snake, (875, 375))
+    screen.blit(apple, (800, 400))
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             game = False
-        elif keys = pygame.key.get_pressed():
+        elif i.type == pygame.KEYDOWN:
                 if i.key == pg.K_LEFT:
                     x -= 15
                 elif i.key == pg.K_RIGHT:
