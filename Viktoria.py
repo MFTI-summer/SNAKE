51a4980292049a5e9397a6a3d2d0a4e56533bb51
microwolnovka27 <86230211+microwolnovka27@users.noreply.github.com>
2021-06-26 10:31:05 +0300

    Update Viktoria.py

--- Viktoria.py
+++ Viktoria.py
@@ -1,47 +1,61 @@
-import pygame
+import pygame as pg
 import sys
+
 #### from pygame.locals import * - чтобы меньше писать
 
+
+pg.init()
+
+### ЭКРАН
+W = 1000  # ширина
+H = 800  # высота
+sc = pg.display.set_mode((W, H))  # длина высота окна
+
+# FPS
 FPS = 60
-W = 1000 #ширина
-H = 800 #высота
-x = 480
-y = 350
-PUR = (100, 128, 255)
+clock = pg.time.Clock()
 
-pygame.init()
-sc = pygame.display.set_mode ((W,H)) #длина высота окна
-pygame.draw.rect(sc, PUR, (x, y, 60, 60), 8)
+# SNAKE HEAD ГОЛОВА ЗМЕИ
 
-clock = pygame.time.Clock()
+PUR = (100, 128, 255)  #ЦВЕТ ЗМЕИ
+
+head = pg.draw.rect(sc, PUR, (480, 350, 60, 60), 8)
+apple = pg.draw.circle(sc, (255, 0,0), (100, 100), 30)
 
 
+speed_x = 0
+speed_y = 0
 
-pygame.display.update() #обновление экрана
+
 
 
 while True:
-
-    for i in pygame.event.get():  # в пг из папки ивент фн гет, присваиает перем зн
-        if i.type == pygame.QUIT:
-            sys.exit()  # кнц
-        elif i.type == pygame.KEYDOWN:
-            if i.key == pygame.K_LEFT:
-                x -= 15
-            elif i.key == pygame.K_RIGHT:
-                x += 15
-            elif i.key == pygame.K_UP:
-                y -= 15
-            elif i.key == pygame.K_DOWN:
-                y += 15
-
-
-    sc.fill((0,0,0))
-    pygame.draw.rect(sc, PUR, (x, y, 60, 60), 8)
     clock.tick(FPS)  # задержка
 
 
-    pygame.display.update()
+    for i in pg.event.get():  # в пг из папки ивент фн гет, присваиает перем зн
+        if i.type == pg.QUIT:
+            sys.exit()  # кнц
+        elif i.type == pg.KEYDOWN:
+            if i.key == pg.K_LEFT:
+                speed_x = -5
 
+            elif i.key == pg.K_RIGHT:
+                speed_x = 5
+
+            elif i.key == pg.K_UP:
+                speed_y = -5
+            elif i.key == pg.K_DOWN:
+                speed_y = 5
+
+    head.x += speed_x
+    head.y += speed_y
+
+    sc.fill((0, 0, 0))
+    pg.draw.rect(sc, PUR, head, 8)
+    pg.draw.circle(sc, (255,0,0),  apple.center, apple.width//2 )
+
+
+    pg.display.update()  # обновление экрана
 
 
